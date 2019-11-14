778abad16c37376c6c43deb08a62e49ebc14de50
Vladislav Smirnov <beluckydaf@gmail.com>
2019-11-14 15:08:42 +0300

    Add limit, and pardon commands. Bans and access.

--- app.js
+++ app.js
@@ -3,25 +3,107 @@ const AWS = require('aws-sdk')
 const Fs = require('fs')
 const Telegram = require('telegraf/telegram')
 
-const token = "<TOKEN>"
-const bot = new Telegraf(token)
-const telegram = new Telegram(token)
+const bot = new Telegraf("<TOKEN>")
+const telegram = new Telegram("<TOKEN>")
 
 const Polly = new AWS.Polly({
     signatureVersion: 'v4',
     region: 'eu-central-1'
 })
 
-const AllowedIds = [146505982, <ID>...]
+const AllowedIds = [146505982, <IDs>...]
 const BeLuckyDaf = 146505982 
 
-bot.start((ctx) => ctx.reply('First, send your ID (' + ctx.message.from.id + ') to @BeLuckyDaf, he will add you to the whitelist.'))
+SelfAllowed = []
+Banned = []
+CharacterLimit = 500
+
+bot.start((ctx) => ctx.reply('First, use the \'/access\' command to get yourself added to the whitelist, type \'/help\' afterwards.'))
 bot.help((ctx) => ctx.reply('Send me a message and I will respond with voice.'))
 
-bot.on('text', (ctx) => {
-        if (!AllowedIds.includes(ctx.message.from.id)) {
-            ctx.reply('Only @BeLuckyDaf and his friends can use this bot, tell him your ID: ' + ctx.message.from.id)
+bot.command('access', (ctx) => {
+        if (AllowedIds.includes(ctx.message.from.id) || SelfAllowed.includes(ctx.message.from.id)) {
+            ctx.reply('You already have access to the bot.')
             return
+        }
+
+        let command = ctx.message.text.split(/[ ]+/)
+        if (command.length < 2) ctx.reply('Do you agree with the rules? [y/n]')
+        else {
+                if (command[1] === 'y' || command[1] === 'Y') {
+                        AllowedIds.push(ctx.message.from.id)
+                        ctx.reply('You are now allowed to use the bot.')
+                } else {
+                        ctx.reply('Okay. Get back when you think you\'re ready.')
+                }
+        }
+})
+
+bot.command('limit', (ctx) => {
+        if (ctx.message.from.id !== BeLuckyDaf) {
+                ctx.reply('Not allowed.')
+                return
+        }
+
+        let command = ctx.message.text.split(/[ ]+/)
+        if (command.length !== 2) {
+                ctx.reply('Wrong argument count.')
+                return
+        }
+
+        let limit = Number(command[1])
+        if (limit < 1 || isNaN(limit)) {
+                ctx.reply('Invalid limit number.')
+                return
+        }
+
+        CharacterLimit = limit
+        ctx.reply(`Character limit set to ${CharacterLimit}.`)
+})
+
+bot.command('pardon', (ctx) => {
+        if (ctx.message.from.id !== BeLuckyDaf) {
+                ctx.reply('Not allowed.')
+                return
+        }
+
+        let command = ctx.message.text.split(/[ ]+/)
+        if (command.length !== 2) {
+                ctx.reply('Wrong argument count.')
+                return
+        }
+
+        let id = Number(command[1])
+        if (id < 1 || isNaN(id)) {
+                ctx.reply('Invalid user id.')
+                return
+        }
+
+        let index = Banned.indexOf(id)
+        if (index === -1) {
+                ctx.reply('User is not banned.')
+                return
+        }
+        Banned.splice(index, 1)
+        ctx.reply(`UserId ${id} was removed from the blacklist.`)
+})
+
+bot.on('text', (ctx) => {
+        if (!AllowedIds.includes(ctx.message.from.id) && !SelfAllowed.includes(ctx.message.from.id)) {
+            ctx.reply('Type \'/access\' to request access to the bot. By using that command, you agree not send text ' +
+                      `larger than ${CharacterLimit} characters.`)
+            return
+        }
+
+        if (ctx.message.text.length > CharacterLimit && !Banned.includes(ctx.message.from.id)) {
+                Banned.push(ctx.message.from.id)
+                ctx.reply(`You are now banned from the bot, (UserId: ${ctx.message.from.id}). If you think it\'s a mistake, write to @BeLuckyDaf.`)
+                return
+        }
+
+        if (Banned.includes(ctx.message.from.id)) {
+                ctx.reply('You are banned. Write to @BeLuckyDaf.')
+                return
         }
 
         let params = {
